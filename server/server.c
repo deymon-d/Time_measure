248552b7352d1f2e3cb1fa0667a7d5a6d31c1942
deymon-d <96654960+deymon-d@users.noreply.github.com>
2023-04-19 21:29:53 +0300

    Update server.c

--- server/server.c
+++ server/server.c
@@ -1,9 +1,11 @@
 #include <fcntl.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 
 #include "../shared/request.h"
+#include "commands.h"
 #include "parser.h"
 #include "user.h"
 
@@ -13,16 +15,46 @@ int main(int argc, char *argv[]) {
         perror("Not found connection\n");
         return 1;
     }
+
+    struct User user = {0};
+    if (!(strcmp(argv[1], "--recovery"))) {
+        recovery(&user);
+        int fd_ans = open("ans.txt", O_CREAT | O_WRONLY, S_IWUSR | S_IRUSR);
+        if (fd_ans == -1) {
+            delete_user(&user);
+            perror("server dead inside\n");
+            return 1;
+        }
+        if (exec_experiment(fd_ans, &user)) {
+            delete_user(&user);
+            perror("Bad connection\n");
+            close(fd_ans);
+            return 1;
+        }
+        close(fd_ans);
+        delete_user(&user);
+        return 0;
+    }
+    prepare_for_work();
     int fd_ans = -1;
-    int fd = open("channel.txt", /*argv[1],*/ O_RDONLY);
+    int fd = open(argv[1], O_RDONLY);
     if (fd == -1) {
         perror("Not found connection\n");
         return 1;
     }
     request_t request = 0;
     ssize_t tmp = 0;
-    struct User user = {0};
     while ((tmp = read(fd, &request, sizeof(request)))) {
+        printf("1\n");
+        if (mod != WORK) {
+            if (mod == SAVE) {
+                work_in_save_mod(&user);
+            } else {
+                work_in_get_mode(&user);
+            }
+            mod = WORK;
+            continue;
+        }
         if (tmp == -1) {
             perror("Bad connection\n");
             close(fd);
@@ -57,6 +89,18 @@ int main(int argc, char *argv[]) {
                 close(fd);
                 return 1;
         }
+    }
+    printf("2\n");
+    sleep(1000);
+    if (mod != WORK) {
+        printf("123\n");
+        if (mod == SAVE) {
+            printf("1923\n");
+            work_in_save_mod(&user);
+        } else {
+            work_in_get_mode(&user);
+        }
+        mod = WORK;
     }
     close(fd);
     delete_user(&user);
